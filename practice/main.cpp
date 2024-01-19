bbfb410ac8372c416ae7d926d7400138c7903bfd
baejae <37645490+baejaeho18@users.noreply.github.com>
2024-01-20 04:52:03 +0900

    Add files via upload

--- practice/main.cpp
+++ practice/main.cpp
@@ -40,6 +40,14 @@ public:
 		fat_start_addr = bytes_per_sector * resv_sector;
 		fat_end_addr = fat_start_addr + fat_size * no_fats;
 	}
+
+	auto test()
+	{
+		cout << fat_size << endl;
+		cout << cluster_size << endl;
+		cout << fat_start_addr << endl;
+		cout << fat_end_addr << endl;
+	}
 };
 
 class Cluster
@@ -48,12 +56,64 @@ public:
 	Cluster(uint8_t* buffer, int size)
 	{
 		sys::io::byte_buffer bb((uint8_t*)buffer, 0, size);
+		int cnt = 0;
+		while (bb.has_remaining())
+		{
+			char buffer_entry[0x20] = { 0 };
+			sys::io::byte_buffer entry = bb.copy_slice(0*cnt++, cnt*0x20);
+			DirEntry dir_entry(entry, 0x20);
+
+		}
+	}
+
+	auto test()
+	{
+
 	}
 };
 
 class DirEntry
 {
+public:
+	string name;
+	string ext;
+	uint8_t attr;
+	uint16_t cl_high;
+	uint16_t cl_low;
+	uint32_t cl_no;
 
+	DirEntry(sys::io::byte_buffer bb, int size)
+	{
+		if (bb.compare_range(0x0B, 1, 0x0F))	// LFN
+		{
+			bb.skip(0x01);
+			name = bb.get_ascii(0x09);
+			bb.skip(0x03);
+			name += bb.get_ascii(0x0C);
+			bb.skip(0x02);
+			name += bb.get_ascii(0x04);
+			attr = 0x0F;
+		}
+		else	// file or directory
+		{
+			name = bb.get_ascii(0x08);
+			ext = bb.get_ascii(0x03);
+			attr = bb.get_uint8();
+			bb.skip(0x08);
+			cl_high = bb.get_uint16_le();
+			bb.skip(0x04);
+			cl_low = bb.get_uint16_le();
+			bb.skip(0x04);
+			cl_no = cl_high << 16 | cl_low;
+		}
+	}
+
+	auto test()
+	{
+		cout << name << "." << ext << endl;
+		cout << attr << endl;
+		cout << cl_no << endl;
+	}
 };
 
 class Fat
@@ -74,10 +134,7 @@ int main(int argc, char* argv[])
   ifs.read(buffer_sb, 0x200);
   byte_buffer bb_sb((uint8_t*)buffer_sb, 0, 0x200);
   Superblock sb((uint8_t*)buffer_sb, 0x200);
-  //cout << sb.fat_size << endl;
-  //cout << sb.cluster_size << endl;
-  //cout << sb.fat_start_addr << endl;
-  //cout << sb.fat_end_addr << endl;
+  sb.test();
 
   // data block
   char buffer_rd[0x1000] = { 0 };
@@ -85,6 +142,7 @@ int main(int argc, char* argv[])
   ifs.read(buffer_rd, 0x1000);
   byte_buffer bb_rd((uint8_t*)buffer_rd, 0, 0x1000);
   Cluster root_dir((uint8_t*)buffer_rd, 0x1000);
+
 
   // fat block
 
